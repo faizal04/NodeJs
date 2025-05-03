4365492334ca0c7a96792e303e0b4dfa84e4d03d
faizal04 <100855720+faizal04@users.noreply.github.com>
2025-05-03 21:51:44 +0530

    deploy

--- Introduction/index.js
+++ Introduction/index.js
@@ -78,6 +78,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('starting the server');
+server.listen(process.env.PORT || 8000, '0.0.0.0', () => {
+  console.log('Server running...');
 });
