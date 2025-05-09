b121d2270b5ef0e7ee8e55d47127b54fdcf80245
faizal04 <100855720+faizal04@users.noreply.github.com>
2025-05-09 22:47:04 +0530

    mounting

---
+++ Natours/routes/tourRoutes.js
@@ -0,0 +1,24 @@
+// app.route('/api/v1/tours').get(GetAllTours).post(CreateTour);
+
+// app
+//   .route('/api/v1/tours/:id')
+//   .get(GetTour)
+//   .patch(UpdateTour)
+//   .delete(DeleteTour);
+
+const express = require('express');
+const tourController = require('../controllers/tourControllers');
+
+const router = express.Router();
+router.param('id', tourController.checkID);
+router
+  .route('/')
+  .get(tourController.GetAllTours)
+  .post(tourController.checkBody, tourController.CreateTour);
+router
+  .route('/:id')
+  .get(tourController.GetTour)
+  .patch(tourController.UpdateTour)
+  .delete(tourController.DeleteTour);
+
+module.exports = router;
