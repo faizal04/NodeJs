83d789f5513981b60045ddfba7aeea7ab7e91611
faizal04 <100855720+faizal04@users.noreply.github.com>
2025-10-30 23:51:29 +0530

    pug overview

--- Natours/routes/viewRoutes.js
+++ Natours/routes/viewRoutes.js
@@ -2,13 +2,7 @@ const express = require('express');
 const viewController = require('./../controllers/viewsController');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.status(200).render('index', {
-    tour: 'the forest hiker',
-    User: 'Faisal Harray',
-  });
-});
 
-router.get('/overview', viewController.getOverview);
+router.get('/', viewController.getOverview);
 router.get('/tour', viewController.getTour);
 module.exports = router;
